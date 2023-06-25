5dfe219570385793c5b203d70fbdd35633b860eb
convitnhodev <hungthai10a1@gmail.com>
2023-06-26 04:58:27 +0700

    fixowner

--- backend/router/router_books.py
+++ backend/router/router_books.py
@@ -20,22 +20,23 @@ def create_new_book(book: BookCreate, db: Session= Depends(get_db), current_user
         code = detail_error.CODE_ERROR_COMOM
         raise HTTPException(status_code = code, 
                             detail = detail_error.map_err[code])
+    
 
 @router.get("")
 def user_list_book(
     offset: int = Query(0, ge=0),
     limit: int = Query(50, ge=1, le=100),
-    db: Session = Depends(get_db)
-):
-    books_return = user_list_book_by_owner(owner="haha", db = db, offset=offset, limit=limit)
+    db: Session = Depends(get_db),
+    current_user: User=Depends(get_current_user_from_token)):
+    books_return = user_list_book_by_owner(owner=current_user.owner, db = db, offset=offset, limit=limit)
     return books_return
 
 
 @router.get("/{id}")
-def user_get_book(id: int,db: Session = Depends(get_db)):
-    book_return = user_get_book_by_id(id=id, db=db, owner="haha")
+def user_get_book(id: int,db: Session = Depends(get_db), current_user: User=Depends(get_current_user_from_token)):
+    book_return = user_get_book_by_id(id=id, db=db, owner=current_user.owner)
     return book_return
 
 @router.delete("/{id}")
-def user_delete_book(id: int, db: Session = Depends(get_db)): 
-    return user_delete_book_by_id(id=id, db=db, owner="haha")
+def user_delete_book(id: int, db: Session = Depends(get_db), current_user: User=Depends(get_current_user_from_token)):
+    return user_delete_book_by_id(id=id, db=db, owner=current_user.owner)
